7bba513c2a7eb97b70929ee4f3ef01fa5d7e2c50
jayeshdhondge <Jayesh.Kulkarni@aaseya.com>
2022-07-06 13:10:48 +0530

    Practice Files Updated

--- Practice/src/practice/Programs.java
+++ Practice/src/practice/Programs.java
@@ -10,13 +10,13 @@ public class Programs {
 	
 	public static void main(String[] args) {
 	
-		String a ="123jayesh";
-		
-	StringBuilder sb = new StringBuilder(a);
-	sb.delete(0, 3);
-	System.out.println(sb);
-	sb.reverse();
-	System.out.println(sb);
+//		String a ="123jayesh";
+//		
+//	StringBuilder sb = new StringBuilder(a);
+//	sb.delete(0, 3);
+//	System.out.println(sb);
+//	sb.reverse();
+//	System.out.println(sb);
 	
 	
 	
